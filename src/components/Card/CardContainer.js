50f1627e6b0c8d8bd7be9531e7d9563ee4c5f44e
Ilkhom Khafizov <ilhomhafizov@MacBook-Pro-Ilhom.local>
2022-12-31 20:54:44 +0500

    fixed some bugs

--- src/components/Card/CardContainer.js
+++ src/components/Card/CardContainer.js
@@ -1,30 +1,26 @@
-import React from 'react'
-import dentist from "../../lotties/dentist.json"
-import cafe from "../../lotties/cafe.json"
-import university from "../../lotties/university.json"
-import hospital from "../../lotties/hospital.json"
-import hotel from "../../lotties/hotel.json"
-import beauty from "../../lotties/beauty.json"
-import shop from "../../lotties/shop.json"
-import student from "../../lotties/student.json"
-import taxi from "../../lotties/taxi.json"
-import train from "../../lotties/train.json"
+import React from "react";
+import dentist from "../../lotties/dentist.json";
+import cafe from "../../lotties/cafe.json";
+import hotel from "../../lotties/hotel.json";
+import beauty from "../../lotties/beauty.json";
+import shop from "../../lotties/shop.json";
+import student from "../../lotties/student.json";
+import taxi from "../../lotties/taxi.json";
+import train from "../../lotties/train.json";
 
-import Card from './Card';
+import Card from "./Card";
 
 export default function CardContainer() {
   return (
     <div className="container mx-auto px-4">
-      <Card lottie={dentist} organizationName={"Стоматология"}/>
-      <Card lottie={cafe} organizationName={"Кафе"}/>
-      {/* <Card lottie={university} organizationName={"Университет"}/>
-      <Card lottie={hospital} organizationName={"Больницы"}/> */}
-      <Card lottie={hotel} organizationName={"Гостиницы"}/>
-      <Card lottie={beauty} organizationName={"Салоны"}/>
-      <Card lottie={shop} organizationName={"Маркеты"}/>
-      <Card lottie={student} organizationName={"Учебные центры"}/>
-      <Card lottie={taxi} organizationName={"Такси"}/>
-      <Card lottie={train} organizationName={"Вокзал"}/>
-</div>
-  )
+      <Card lottie={dentist} organizationName={"Стоматология"} />
+      <Card lottie={cafe} organizationName={"Кафе"} />
+      <Card lottie={hotel} organizationName={"Гостиницы"} />
+      <Card lottie={beauty} organizationName={"Салоны"} />
+      <Card lottie={shop} organizationName={"Маркеты"} />
+      <Card lottie={student} organizationName={"Учебные центры"} />
+      <Card lottie={taxi} organizationName={"Такси"} />
+      <Card lottie={train} organizationName={"Вокзал"} />
+    </div>
+  );
 }
